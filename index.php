aa1f7b443237ad44e9c629ca8b5451ebfd4e8b7c
sid721 <59916267+sid721@users.noreply.github.com>
2020-01-17 18:26:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
+<?php include("DB.php"); ?>
 <?php 
-
 $method = $_SERVER['REQUEST_METHOD'];
 
 // Process only when method is POST
@@ -8,13 +8,34 @@ if($method == 'POST'){
 	$requestBody = file_get_contents("php://input");
 	$json = json_decode($requestBody, true);
 	$Empname = $json["queryResult"]["parameters"]["Empname"];
-	if($Empname != "" && $Empname != null)
+	$NumberType = $json["queryResult"]["parameters"]["Numbertype"];
+	if(($Empname != "" && $Empname != null) && ($NumberType != "" && $NumberType != null))
 	{
-		$speech = "Ya sure, which type of number you want intercom number , telephone number or mobile number";
+		$sql="Select * from emp where Empname LIKE '%$Empname%' ";
+		$result = mysqli_query($connection, $sql) or die("Error " . mysqli_error($connection));
+        $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+        $Mobile = $row['Mobile'];
+		$Intercome = $row['Intercome'];
+		$telephone = $row['telephone'];
+		$speech = "Mobile Number is ".$Mobile." , Intercome Number is ".$Intercome." , Telephone Number is ".$telephone;
 	}
-	else
+	else 
 	{
-		$speech = "Ya sure, May i have the employee name or emailid";
+		if(($Empname != "" && $Empname != null) && ($NumberType == "" || $NumberType == null)
+		{
+			$speech = "which type of number you want intercom number , telephone number or mobile number";
+		}		
+		else
+		{
+			if(($Empname == "" || $Empname == null) && ($NumberType != "" && $NumberType != null)
+			{
+				$speech = "which type of number you want intercom number , telephone number or mobile number";
+			}
+			else
+			{
+				$speech = "May i have the employee name please";
+			}
+		}
 	}
 
 	$response = new \stdClass();
