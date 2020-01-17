47af07244a1e7c04e95c4c53dffb5fd1728529d6
sid721 <59916267+sid721@users.noreply.github.com>
2020-01-17 18:28:36 +0530

    Create DB.php

---
+++ DB.php
@@ -0,0 +1,20 @@
+<?php
+//connect to mysql database
+//$connection = mysqli_connect("HOSTNAME","DBNAME","PASSWORD","USER") or die("Error " . mysqli_error($connection));
+//$connection = mysqli_connect('localhost','IRFCDB2','Irfc@2018dl2c6656','root') or die("Connection fail... " . mysqli_error($connection));
+
+/* check connection 
+if (!$connection) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+*/
+?>
+<?php
+$dbname = 'ncrtc';
+$dbuser = 'root';
+$dbpass = 'bJdHRPpPK1vn';
+$dbhost = 'localhost';
+$connection = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
+mysqli_select_db($connection, $dbname) or die("Could not open the db '$dbname'");
+?>
