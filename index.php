d3aff0af4239a389f076c46d843736b91c762903
Bhupendra-Kumar <Bhupeekr.77777@gmail.com>
2020-03-08 22:23:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,34 +9,39 @@ if($method == 'POST'){
 	$json = json_decode($requestBody, true);
 	$Empname = $json["queryResult"]["parameters"]["Empname"];
 	$NumberType = $json["queryResult"]["parameters"]["Numbertype"];
-	if(($Empname != "" && $Empname != null) && ($NumberType != "" && $NumberType != null))
-	{
-		$sql="Select * from emp where empname LIKE '%$Empname%' ";
-		$result = mysqli_query($connection, $sql) or die("Error " . mysqli_error($connection));
-        	$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-        	$Mobile = $row['mobile'];
-		$Intercome = $row['intercome'];
-		$telephone = $row['telephone'];
-		$speech = "Mobile Number is ".$Mobile." , Intercome Number is ".$Intercome." , Telephone Number is ".$telephone;
-	}
-	else 
-	{
-		if(($Empname != "" && $Empname != null) && ($NumberType == "" || $NumberType == null))
-		{
-			$speech = "which type of number you want intercom number , telephone number or mobile number";
-		}		
-		else
-		{
-			if(($Empname == "" || $Empname == null) && ($NumberType != "" && $NumberType != null))
-			{
-				$speech = "which type of number you want intercom number , telephone number or mobile number";
-			}
-			else
-			{
-				$speech = "May i have the employee name please";
-			}
-		}
-	}
+// 	$sql="Select * from emp where empname LIKE '%$Empname%' ";
+// 	$result = mysqli_query($connection, $sql) or die("Error " . mysqli_error($connection));
+//         $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+	$speech = "api working fine";
+	
+// 	if(($Empname != "" && $Empname != null) && ($NumberType != "" && $NumberType != null))
+// 	{
+// 		$sql="Select * from emp where empname LIKE '%$Empname%' ";
+// 		$result = mysqli_query($connection, $sql) or die("Error " . mysqli_error($connection));
+//         	$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+//         	$Mobile = $row['mobile'];
+// 		$Intercome = $row['intercome'];
+// 		$telephone = $row['telephone'];
+// 		$speech = "Mobile Number is ".$Mobile." , Intercome Number is ".$Intercome." , Telephone Number is ".$telephone;
+// 	}
+// 	else 
+// 	{
+// 		if(($Empname != "" && $Empname != null) && ($NumberType == "" || $NumberType == null))
+// 		{
+// 			$speech = "which type of number you want intercom number , telephone number or mobile number";
+// 		}		
+// 		else
+// 		{
+// 			if(($Empname == "" || $Empname == null) && ($NumberType != "" && $NumberType != null))
+// 			{
+// 				$speech = "which type of number you want intercom number , telephone number or mobile number";
+// 			}
+// 			else
+// 			{
+// 				$speech = "May i have the employee name please";
+// 			}
+// 		}
+// 	}
 
 	$response = new \stdClass();
 	$response->fulfillmentText = $speech;
